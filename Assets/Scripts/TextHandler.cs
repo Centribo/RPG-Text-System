945768b3ac23118afae9ca113f951ce2b2d59561
Adam Leung <centribo@hotmail.com>
2015-12-04 18:36:29 -0500

    Initial Commit

---
+++ Assets/Scripts/TextHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHandler : MonoBehaviour {
+
+	//Public variables
+	public string text;
+
+	//Private variables
+	AudioSource bleepSound;
+
+	void Awake(){
+		bleepSound = GetComponent<AudioSource>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
