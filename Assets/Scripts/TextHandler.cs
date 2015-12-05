3cfdc6845942f806c658f6dff289c4830476fdda
Adam Leung <centribo@hotmail.com>
2015-12-04 21:51:09 -0500

    Implementation complete. TODO: Comments

--- Assets/Scripts/TextHandler.cs
+++ Assets/Scripts/TextHandler.cs
@@ -4,17 +4,11 @@ using System.Collections;
 
 public class TextHandler : MonoBehaviour {
 
-	public enum Pitches {
-		Low = 1,
-		High = 2
-	}
-
 	public enum States {
-		Unloaded, //Array is not loaded with text yet
+		Unloaded, //Array is not loaded with text yet, or we have displayed all lines of text
 		Loaded, //Array is loaded with text, but we're not displaying yet
-		Talking, //We are revealing character-by-character to display a new line of dialogue
+		Revealing, //We are revealing character-by-character to display a new line of dialogue
 		Displaying, //We are done going through characters for a line
-		Complete //We're done displaying all lines in given array of text
 	}
 
 	//Public variables
@@ -25,7 +19,7 @@ public class TextHandler : MonoBehaviour {
 
 	//Private variables
 	int stringIndex; //Index of string in array we are displaying
-	int characterIndex; //Index of character to reveal
+	int charIndex; //Index of character to reveal
 	AudioSource bleepSound; //The base "bleep" sound for revealing a character
 	float timer = 0; //Timer for going character-by-character
 
@@ -33,37 +27,81 @@ public class TextHandler : MonoBehaviour {
 		bleepSound = GetComponent<AudioSource>();
 		//state = States.Unloaded;
 		state = States.Loaded;
-		bleepSound.pitch = (float)Pitches.Low;
 	}
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		
+		UpdateCharacter();
+
+		 if (Input.GetMouseButtonUp(0)){
+		 	AdvanceLine();
+		 }
 	}
 
 	public void SetText(string[] newText){ //Used to update new text
 		text = newText; //Set new text
 		stringIndex = 0; //Reset indicies
-		characterIndex = 0;
+		charIndex = 0;
 	}
 
 	public void SetRate(float r){ //Used to set the rate of revealing characters
 		rate = r;
 	}
 
-	public void DisplayNextLine(){
+	public void AdvanceLine(){ //Called to skip revealing, or initiate revealing of next line.
+		switch(state){
+			case States.Loaded:
+				state = States.Revealing;
+			break;
+			case States.Revealing:
+				state = States.Displaying;
+				charIndex = text[stringIndex].Length;
+				UpdateText();
+			break;
+			case States.Displaying:
+				stringIndex ++;
+				if(stringIndex > text.Length - 1){
+					UpdateText("");
+					state = States.Unloaded;
+				} else {
+					state = States.Revealing;
+					charIndex = 0;
+				}
+			break;
+			case States.Unloaded:
+				Debug.Log("All given text has been displayed.");
+			break;
+		}
+	}
 
+	public void AdvanceCharacter(){ //Called to advance character index
+		charIndex ++;
+		if(charIndex >= text[stringIndex].Length){
+			charIndex = text[stringIndex].Length;
+			state = States.Displaying;
+		}
+	}
+
+	public void UpdateText(){
+		UpdateText(text[stringIndex].Substring(0, charIndex));
+	}
+
+	public void UpdateText(string s){
+		textBox.text = s;
+		Debug.Log(s);
 	}
 
 	void UpdateCharacter(){ //Used to reveal another character
-		if(timer >= rate){
+		if(state == States.Revealing && timer >= rate){
 			timer = 0;
+			AdvanceCharacter();
+			UpdateText();
+			bleepSound.pitch = Random.Range(1, 5);
 			bleepSound.Play();
 		}
 	}
